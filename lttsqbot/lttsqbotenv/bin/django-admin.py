8d05107c140f46fc38445aab7887e64fdb16c950
sangeeta2306 <sangeeta.bose.236@gmail.com>
2018-11-23 19:36:58 +0530

    Changes

---
+++ lttsqbot/lttsqbotenv/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/pcadmin/codebusters/lttsqbot/lttsqbotenv/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.8.19','django-admin.py'
+__requires__ = 'Django==1.8.19'
+__import__('pkg_resources').run_script('Django==1.8.19', 'django-admin.py')
